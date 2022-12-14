d7d8a2b9204e746836210b5c5c6724a1a3ba9c58
Jared lane <robertjaredlane5@gmail.com>
2022-12-14 15:44:56 -0500

    Initial Commit

---
+++ Apple Game/Apple_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_GameApp.swift
+//  Apple Game
+//
+//  Created by Jared Lane on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
